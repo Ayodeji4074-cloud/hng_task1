abbd5c1b7410ff717b2ae50c6792cc0b0d5e3484
Ayodeji4074-cloud <adeniyiemmanuel998@gmail.com>
2024-07-01 13:28:11 +0100

    hng

--- index.js
+++ index.js
@@ -3,12 +3,18 @@ const axios = require('axios');
 const app = express();
 const port = process.env.PORT || 3000;
 
+// Handle the root URL
+app.get('/', (req, res) => {
+    res.send('Welcome to the API server. Use /api/hello?visitor_name=Emmanuel');
+});
+
+// Handle the test api endpoint
 app.get('/api/hello', async (req, res) => {
-    const visitorName = req.query.visitor_name || "John";
+    const visitorName = req.query.visitor_name || "Emmanuel";
     const clientIp = req.headers['x-forwarded-for'] || req.socket.remoteAddress;
 
     try {
-        // Using WeatherAPI to get location and weather data
+        // Using WeatherAPI to get my location and weather data
         const apiKey = 'f8a69c8497b943b8a2883252240107';
         const weatherApiResponse = await axios.get(`http://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${clientIp}`);
         const weatherData = weatherApiResponse.data;
