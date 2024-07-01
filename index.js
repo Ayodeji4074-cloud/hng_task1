5b8b820ff4f33511f1b96c820671ae34efa5d808
Ayodeji4074-cloud <adeniyiemmanuel998@gmail.com>
2024-07-01 13:09:22 +0100

    hng

--- index.js
+++ index.js
@@ -3,18 +3,13 @@ const axios = require('axios');
 const app = express();
 const port = process.env.PORT || 3000;
 
-// Add this route to handle the root URL
-app.get('/', (req, res) => {
-    res.send('Welcome to the API server. Use /api/hello?visitor_name=John');
-});
-
 app.get('/api/hello', async (req, res) => {
     const visitorName = req.query.visitor_name || "John";
     const clientIp = req.headers['x-forwarded-for'] || req.socket.remoteAddress;
 
     try {
-        // Use WeatherAPI to get location and weather data
-        const apiKey = 'f8a69c8497b943b8a2883252240107'; // Replace with your WeatherAPI key
+        // Using WeatherAPI to get location and weather data
+        const apiKey = 'f8a69c8497b943b8a2883252240107';
         const weatherApiResponse = await axios.get(`http://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${clientIp}`);
         const weatherData = weatherApiResponse.data;
         const city = weatherData.location.name;
@@ -32,4 +27,4 @@ app.get('/api/hello', async (req, res) => {
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
-}); 
+});
